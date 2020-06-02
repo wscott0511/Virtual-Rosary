5c10fabd862b7cbca89480c6fe2b3aff6a3d4d76
Ryan Scott <williamscott0511@gmail.com>
2020-06-02 13:16:14 -0700

    First Commit

---
+++ Virtual Rosary/Virtual Rosary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Rosary
+//
+//  Created by William Scott on 6/2/20.
+//  Copyright © 2020 William Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
